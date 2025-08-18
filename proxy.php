20b8bdd79144141e518ffcb6397eb269d553121c
theyanaxj <yanathip1317@gmail.com>
2025-08-19 04:13:29 +0700

    Add files via upload

--- proxy.php
+++ proxy.php
@@ -1,17 +1,23 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *'); // Allow all origins for this example
+header('Access-Control-Allow-Origin: *');
 
-$api_key = "ec31861437ccd5379e2c1c78c01b2bce"; // Your API Key
+
+$api_key = "ec31861437ccd5379e2c1c78c01b2bce"; 
+
+
 $endpoint = $_GET['endpoint'];
-$params = http_build_query(array_merge($_GET, ['api_key' => $api_key]));
+$params = http_build_query(array_merge($_GET, ['api_key' => $api_key, 'language' => 'th-TH']));
+
 
 $url = "https://api.themoviedb.org/3" . $endpoint . "?" . $params;
+
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
+
 
 echo $response;
 ?>
